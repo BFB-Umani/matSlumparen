dee80ddb01842bcfd0f1dedbed46d187f68aa466
Cornelia Bea Persson <corneliap@live.se>
2020-08-18 16:18:59 +0200

    Simple menu

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Välj:\n1 - Veganskt\n2 - Ej veganskt");
+        int choiceVeganOrNot = scanner.nextInt();
+        System.out.println("Välj:\n1 - Lunch\n2 - Middag\n3 - Dessert");
+        int choiceTypeOfSetting = scanner.nextInt();
+
+        // För att göra mer avancerat: fler val. Nyttigt/snabbt/billigt osv
+    }
 }
