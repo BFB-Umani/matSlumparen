db602189f23222ff68fbc342fc7223c37138af32
Cornelia Bea Persson <corneliap@live.se>
2020-08-18 16:43:02 +0200

    added Food-class

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,10 @@ public class Main {
         int choiceVeganOrNot = scanner.nextInt();
         System.out.println("Välj:\n1 - Lunch\n2 - Middag\n3 - Dessert");
         int choiceTypeOfSetting = scanner.nextInt();
+        String dish = "pizza med svamp och parmesanost";
+        String setting = "middag";
+        System.out.println("Vi rekommenderar att du äter " + dish + " till " + setting);
 
-        // För att göra mer avancerat: fler val. Nyttigt/snabbt/billigt osv
+        // För att göra mer avancerat: fler val. Nyttigt/snabbt/billigt/allergier osv
     }
 }
