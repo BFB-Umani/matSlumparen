9d16f734071c2ebfbac0610fffb1dfbfa9ae5fcb
Cornelia Bea Persson <corneliap@live.se>
2020-08-18 17:12:05 +0200

    variables in Food-class with getters and setters

--- src/Food.java
+++ src/Food.java
@@ -1,2 +1,30 @@
 public class Food {
+
+    String name;
+    int id;
+    String linkToRecipe;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLinkToRecipe() {
+        return linkToRecipe;
+    }
+
+    public void setLinkToRecipe(String linkToRecipe) {
+        this.linkToRecipe = linkToRecipe;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,10 @@ public class Main {
         int choiceVeganOrNot = scanner.nextInt();
         System.out.println("Välj:\n1 - Lunch\n2 - Middag\n3 - Dessert");
         int choiceTypeOfSetting = scanner.nextInt();
-        String dish = "pizza med svamp och parmesanost";
+        String dish = "smördegspizza med spenat, svamp och parmesanost";
         String setting = "middag";
-        System.out.println("Vi rekommenderar att du äter " + dish + " till " + setting);
+        String link = "https://mittkok.expressen.se/recept/smordegspizza-med-spenat-svamp-parmesan/";
+        System.out.println("Vi rekommenderar att du äter " + dish + " till " + setting + ".\nLänk till recept: " + link);
 
         // För att göra mer avancerat: fler val. Nyttigt/snabbt/billigt/allergier osv
     }
