ce6270743d9640ff315be519678640b58b46a092
Cornelia Bea Persson <corneliap@live.se>
2020-08-20 14:44:30 +0200

    adding food i FoodDatabase and changing everything to english

--- src/FoodDatabase.java
+++ src/FoodDatabase.java
@@ -8,33 +8,35 @@ public class FoodDatabase {
     private List<Food> veganFoodList = new ArrayList<>();
 
 
-    public void initVegDB(){
-        veganFoodList.add(new Food(1,"Sallad", true, Arrays.asList(Category.LUNCH, Category.DINNER), ""));
-        veganFoodList.add(new Food(2,"Tofu",true,Arrays.asList(Category.DINNER),""));
-        veganFoodList.add(new Food(3, "FruktSallad", true, Arrays.asList(Category.LUNCH, Category.DINNER, Category.DESSERT), ""));
-        veganFoodList.add(new Food(4,"Vegan Burger", true, Arrays.asList(Category.LUNCH, Category.DINNER), ""));
-        veganFoodList.add(new Food(6, "Bibimbap", true, Arrays.asList(Category.LUNCH, Category.DINNER),"https://www.lazycatkitchen.com/vegan-bibimbap/"));
-        veganFoodList.add(new Food(9, "Vegan kebab", true, Arrays.asList(Category.LUNCH, Category.DINNER),"https://www.veganfoodandliving.com/features/13-vegan-kebab-and-skewer-recipes-for-your-next-bbq-party/"));
-        veganFoodList.add(new Food(10, "Vegan Cheesecake", true, Arrays.asList(Category.DESSERT),"https://minimalistbaker.com/7-ingredient-vegan-cheesecakes/"));
+    public void initVegDB() {
+        veganFoodList.add(new Food(1, "Salad", true, Arrays.asList(Category.LUNCH, Category.DINNER), ""));
+        veganFoodList.add(new Food(2, "Tofu", true, Arrays.asList(Category.DINNER), ""));
+        veganFoodList.add(new Food(3, "Fruitsalad", true, Arrays.asList(Category.LUNCH, Category.DINNER, Category.DESSERT), ""));
+        veganFoodList.add(new Food(4, "Vegan Burger", true, Arrays.asList(Category.LUNCH, Category.DINNER), ""));
+        veganFoodList.add(new Food(6, "Bibimbap", true, Arrays.asList(Category.LUNCH, Category.DINNER), "https://www.lazycatkitchen.com/vegan-bibimbap/"));
+        veganFoodList.add(new Food(9, "Vegan kebab", true, Arrays.asList(Category.LUNCH, Category.DINNER), "https://www.veganfoodandliving.com/features/13-vegan-kebab-and-skewer-recipes-for-your-next-bbq-party/"));
+        veganFoodList.add(new Food(10, "Vegan Cheesecake", true, Arrays.asList(Category.DESSERT), "https://minimalistbaker.com/7-ingredient-vegan-cheesecakes/"));
     }
 
-    public void initMeatDb(){
-        normalFoodList.add(new Food(5,"Hot dog", false, Arrays.asList(Category.LUNCH, Category.DINNER), ""));
-        normalFoodList.add(new Food(6,"Lasagne", false, Arrays.asList(Category.DINNER), ""));
-        normalFoodList.add(new Food(7,"Köttlimpa", false, Arrays.asList(Category.DINNER), ""));
-        normalFoodList.add(new Food(8, "Apple pie", false, Arrays.asList(Category.DESSERT), ""));
-        normalFoodList.add(new Food(9, "Tiramisu", false, Arrays.asList(Category.DESSERT), ""));
-        normalFoodList.add(new Food(8, "Pork Dumplings", false, Arrays.asList(Category.LUNCH, Category.DINNER),"https://www.thekitchn.com/how-to-make-pork-dumplings-cooking-lessons-from-the-kitchn-216163"));
+    public void initMeatDb() {
+        normalFoodList.add(new Food(5, "Hot dog", false, Arrays.asList(Category.LUNCH, Category.DINNER), ""));
+        normalFoodList.add(new Food(12, "Lasagne", false, Arrays.asList(Category.DINNER), "https://www.arla.se/recept/lasagne/"));
+        normalFoodList.add(new Food(7, "Meatloaf", false, Arrays.asList(Category.DINNER), ""));
+        normalFoodList.add(new Food(13, "Apple pie", false, Arrays.asList(Category.DESSERT), ""));
+        normalFoodList.add(new Food(14, "Tiramisu", false, Arrays.asList(Category.DESSERT), ""));
+        normalFoodList.add(new Food(15, "Pork Dumplings", false, Arrays.asList(Category.LUNCH, Category.DINNER), "https://www.thekitchn.com/how-to-make-pork-dumplings-cooking-lessons-from-the-kitchn-216163"));
+        normalFoodList.add(new Food(16, "Penne vodka", false, Arrays.asList(Category.DINNER), "https://www.aftonbladet.se/matdryck/a/qLe41E/penne-vodka--pastaratten-med-sting"));
+        normalFoodList.add(new Food(17, "Chokladmousse", false, Arrays.asList(Category.DESSERT), "https://www.arla.se/recept/chokladmousse/"));
 
     }
 
     public Food getRandomFoodFromCategory(Category category, FoodDatabase foodDatabase, Boolean isVegan) {
         List<Food> foodList;
 
-        if(isVegan){
+        if (isVegan) {
             initVegDB();
             foodList = getVeganFoodList();
-        } else{
+        } else {
             initMeatDb();
             foodList = getNormalFoodList();
         }
